d8959e8ddb0eea5e4334b06d7736f732f0175f46
Mateusz Jaje <mateuszjaje@gmail.com>
2014-11-13 12:09:43 +0100

    częściowo zaimplementowany podpunkt 3 z lab3

--- src/main/scala/io/github/morgaroth/reactive/lab2/actors/Buyer.scala
+++ src/main/scala/io/github/morgaroth/reactive/lab2/actors/Buyer.scala
@@ -2,20 +2,22 @@ package io.github.morgaroth.reactive.lab2.actors
 
 import akka.actor._
 import akka.event.LoggingReceive
-import io.github.morgaroth.reactive.lab2.actors.AuctionSearch.{SearchResults, Search}
+import io.github.morgaroth.reactive.lab2.actors.AuctionSearch.{Search, SearchResults}
 import io.github.morgaroth.reactive.lab2.actors.auction.AuctionActorMessages._
+import io.github.morgaroth.reactive.lab2.utils.randomsDSL
 import pl.morgaroth.utils.random.randoms
 
 import scala.concurrent.duration._
-import scala.util.Random
 
-class Buyer(target: String) extends Actor with ActorLogging with randoms {
+class Buyer(target: String) extends Actor with ActorLogging with randoms with randomsDSL {
 
   import context._
 
   val auctionSearch = context actorSelection "/user/AuctionSearch"
 
   auctionSearch ! Search(target)
+
+  val maxPrice: Double = 40 ~ 80 random()
 
   var boughtCTR = 0
   var items: List[ActorRef] = _
@@ -35,18 +37,30 @@ class Buyer(target: String) extends Actor with ActorLogging with randoms {
       context become working
   }
 
-
   def working = LoggingReceive {
     case OK =>
     case Beaten(price) =>
       val send = sender()
-      context.system.scheduler.scheduleOnce((randomInt(200) + 100) millis) {
-        send.!(Offer(price + randGenerator.nextDouble()))(self)
+      if (price < maxPrice) {
+        context.system.scheduler.scheduleOnce((randomInt(200) + 100) millis) {
+          send.!(Offer(price + randGenerator.nextDouble()))(self)
+        }
+      } else {
+        boughtCTR += 1
+        log.info(s"I not bought from ${sender().path.name} because price is to high")
+        stopIfEnd()
       }
+
     case NotEnough(price) =>
       val send = sender()
-      context.system.scheduler.scheduleOnce((randomInt(200) + 100) millis) {
-        send.tell(Offer(price + randGenerator.nextDouble()), self)
+      if (price < maxPrice) {
+        context.system.scheduler.scheduleOnce((randomInt(200) + 100) millis) {
+          send.tell(Offer(price + randGenerator.nextDouble()), self)
+        }
+      } else {
+        boughtCTR += 1
+        log.info(s"I not bought from ${sender().path.name} because price is to high")
+        stopIfEnd()
       }
 
     case ItemSold(item) =>
