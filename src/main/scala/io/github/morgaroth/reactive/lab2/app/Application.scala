ff8acc5a15a17fcee74754c9fe1a20f2fd7ff836
Mateusz Jaje <mateuszjaje@gmail.com>
2014-11-13 12:17:55 +0100

    poprawki w kodzie

--- src/main/scala/io/github/morgaroth/reactive/lab2/app/Application.scala
+++ src/main/scala/io/github/morgaroth/reactive/lab2/app/Application.scala
@@ -6,41 +6,32 @@ import io.github.morgaroth.reactive.lab2.actors.SoulsReaper.WatchHim
 import io.github.morgaroth.reactive.lab2.actors.{AuctionSearch, Buyer, Seller, SoulsReaper}
 import io.github.morgaroth.reactive.lab2.utils.MapOfArraysConfigReader
 import net.ceedubs.ficus.Ficus._
-import akka.event.{Logging,LoggingAdapter}
+import akka.event.{Logging, LoggingAdapter}
 
 import scala.concurrent.duration._
 
 object Application extends MapOfArraysConfigReader {
 
   class Reaper extends SoulsReaper {
-    // Derivations need to implement this method.  It's the
     override def allSoulsReaped(): Unit = context.system.shutdown()
   }
-
 
   def main(args: Array[String]) {
     val system = ActorSystem("Lydie")
     import system.dispatcher
 
-    val log = Logging(system,"APP")
+    val reaper = system.actorOf(Props[Reaper], "reaper")
 
-    val reaper = system.actorOf(Props[Reaper],"reaper")
-
-    val buyers = readMapStringAtPath("reactive.lab2.buyers")
-
-    val sellers = readMapListStringAtPath("reactive.lab2.sellers").map { x =>
+    readMapListStringAtPath("reactive.lab2.sellers").map { x =>
       val (name, items) = x
       val seller = system.actorOf(Props(classOf[Seller], items), s"seller_$name")
       reaper ! WatchHim(seller)
-      seller
     }
 
-    val auctionSearchActor = system.actorOf(Props[AuctionSearch], "AuctionSearch")
+    system.actorOf(Props[AuctionSearch], "AuctionSearch")
 
-    println(auctionSearchActor.path)
-
-    system.scheduler.scheduleOnce(2 seconds) {
-      buyers.map { x =>
+    system.scheduler.scheduleOnce(1 seconds) {
+      readMapStringAtPath("reactive.lab2.buyers").map { x =>
         val (name, target) = x
         val buyer = system.actorOf(Props(classOf[Buyer], target), name)
         reaper ! WatchHim(buyer)
